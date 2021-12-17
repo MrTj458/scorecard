ac51a23571e925ef720969aafabadbd96cb27a21
MrTj458 <mrtj458@gmail.com>
2021-12-16 20:40:27 -0700

    Scorecard create

--- controllers/scorecards.go
+++ controllers/scorecards.go
@@ -1,7 +1,11 @@
 package controllers
 
 import (
+	"fmt"
+	"net/http"
+
 	"github.com/MrTj458/scorecard/models"
+	"github.com/MrTj458/scorecard/views"
 	"github.com/go-chi/chi/v5"
 )
 
@@ -18,5 +22,68 @@ func NewScorecards(service *models.ScorecardService) *Scorecards {
 func (s *Scorecards) Routes() *chi.Mux {
 	r := chi.NewRouter()
 
+	r.Post("/", s.create)
+	r.Get("/", s.findAll)
+	r.Get("/{id}", s.findByID)
+
 	return r
+}
+
+func (sc *Scorecards) create(w http.ResponseWriter, r *http.Request) {
+	var s models.ScorecardIn
+	if err := views.DecodeJSON(r.Body, &s); err != nil {
+		views.Error(w, http.StatusUnprocessableEntity, "invalid scorecard object received")
+		return
+	}
+
+	if vErrors := views.Validate(s); vErrors != nil {
+		views.FieldError(w, http.StatusUnprocessableEntity, "invalid scorecard object received", vErrors)
+		return
+	}
+
+	id, err := sc.s.Add(s)
+	if err != nil {
+		views.Error(w, http.StatusInternalServerError, "unable to create new user")
+		return
+	}
+
+	ret, err := sc.s.FindByID(id)
+	if err != nil {
+		views.Error(w, http.StatusInternalServerError, "unable to find created scorecard")
+		return
+	}
+
+	views.JSON(w, http.StatusCreated, ret)
+}
+
+func (sc *Scorecards) findAll(w http.ResponseWriter, r *http.Request) {
+	user := r.URL.Query().Get("user")
+
+	var scorecards []models.Scorecard
+	var err error
+
+	if len(user) == 0 {
+		scorecards, err = sc.s.FindAll()
+	} else {
+		scorecards, err = sc.s.FindAllByUserId(user)
+	}
+
+	if err != nil {
+		views.Error(w, http.StatusInternalServerError, "error retrieving scorecards")
+		return
+	}
+
+	views.JSON(w, http.StatusOK, scorecards)
+}
+
+func (sc *Scorecards) findByID(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+
+	s, err := sc.s.FindByID(id)
+	if err != nil {
+		views.Error(w, http.StatusNotFound, fmt.Sprintf("scorecard with id '%s' not found", id))
+		return
+	}
+
+	views.JSON(w, http.StatusOK, s)
 }
--- models/scorecard.go
+++ models/scorecard.go
@@ -3,32 +3,48 @@ package models
 import (
 	"time"
 
+	"github.com/MrTj458/scorecard/db"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type Scorecard struct {
 	ID          primitive.ObjectID `json:"id" bson:"_id"`
-	CreatedBy   primitive.ObjectID `json:"created_by"`
-	StartTime   time.Time          `json:"start_time"`
-	EndTime     time.Time          `json:"end_time"`
-	NumHoles    int                `json:"num_holes"`
-	CourseName  string             `json:"course_name"`
-	CourseState string             `json:"course_state"`
-	Players     []struct {
-		Username string             `json:"username"`
-		ID       primitive.ObjectID `json:"id"`
-	} `json:"players"`
-	Holes []struct {
-		Hole     int `json:"hole"`
-		Par      int `json:"par"`
-		Distance int `json:"distance"`
-		Scores   []struct {
-			Username string             `json:"username"`
-			ID       primitive.ObjectID `json:"id"`
-			Strokes  int                `json:"strokes"`
-		} `json:"scores"`
-	} `json:"holes"`
+	CreatedBy   primitive.ObjectID `json:"created_by" bson:"created_by"`
+	StartTime   time.Time          `json:"start_time" bson:"start_time"`
+	EndTime     *time.Time         `json:"end_time" bson:"end_time"`
+	NumHoles    int                `json:"num_holes" bson:"num_holes"`
+	CourseName  string             `json:"course_name" bson:"course_name"`
+	CourseState string             `json:"course_state" bson:"course_state"`
+	Players     []player           `json:"players" bson:"players"`
+	Holes       []hole             `json:"holes" bson:"holes"`
+}
+
+type player struct {
+	Username string             `json:"username" validate:"required" bson:"username"`
+	ID       primitive.ObjectID `json:"id" validate:"required" bson:"id"`
+}
+
+type hole struct {
+	Hole     int     `json:"hole" validate:"required" bson:"hole"`
+	Par      int     `json:"par" validate:"required" bson:"par"`
+	Distance int     `json:"distance" validate:"required" bson:"distance"`
+	Scores   []score `json:"scores" validate:"required" bson:"scores"`
+}
+
+type score struct {
+	Username string             `json:"username" validate:"required" bson:"username"`
+	ID       primitive.ObjectID `json:"id" validate:"required" bson:"id"`
+	Strokes  int                `json:"strokes" validate:"required" bson:"strokes"`
+}
+
+type ScorecardIn struct {
+	CreatedBy   primitive.ObjectID `json:"created_by" validate:"required"`
+	NumHoles    int                `json:"num_holes" validate:"required"`
+	CourseName  string             `json:"course_name" validate:"required"`
+	CourseState string             `json:"course_state" validate:"required"`
+	Players     []player           `json:"players" validate:"required"`
 }
 
 type ScorecardService struct {
@@ -41,4 +57,87 @@ func NewScorecardService(db *mongo.Database) *ScorecardService {
 		db:   db,
 		coll: db.Collection("scorecards"),
 	}
+}
+
+func (ss *ScorecardService) Add(scorecard ScorecardIn) (string, error) {
+	s := Scorecard{
+		ID:          primitive.NewObjectID(),
+		CreatedBy:   scorecard.CreatedBy,
+		StartTime:   time.Now().UTC(),
+		EndTime:     nil,
+		NumHoles:    scorecard.NumHoles,
+		CourseName:  scorecard.CourseName,
+		CourseState: scorecard.CourseState,
+		Players:     scorecard.Players,
+		Holes:       make([]hole, 0),
+	}
+
+	_, err := ss.coll.InsertOne(db.Ctx, s)
+	if err != nil {
+		return "", err
+	}
+
+	return s.ID.Hex(), nil
+}
+
+func (ss *ScorecardService) FindAll() ([]Scorecard, error) {
+	cur, err := ss.coll.Find(db.Ctx, bson.D{})
+	if err != nil {
+		return nil, err
+	}
+	defer cur.Close(db.Ctx)
+
+	var scorecards []Scorecard
+	if err = cur.All(db.Ctx, &scorecards); err != nil {
+		return nil, err
+	}
+
+	if scorecards == nil {
+		scorecards = make([]Scorecard, 0)
+	}
+
+	return scorecards, nil
+}
+
+func (ss *ScorecardService) FindByID(id string) (Scorecard, error) {
+	oId, err := primitive.ObjectIDFromHex(id)
+	if err != nil {
+		return Scorecard{}, err
+	}
+
+	var s Scorecard
+	err = ss.coll.FindOne(db.Ctx, bson.D{{"_id", oId}}).Decode(&s)
+	if err != nil {
+		return Scorecard{}, err
+	}
+
+	return s, nil
+}
+
+func (ss *ScorecardService) FindAllByUserId(userId string) ([]Scorecard, error) {
+	uId, err := primitive.ObjectIDFromHex(userId)
+	if err != nil {
+		return nil, err
+	}
+
+	filter := bson.D{
+		{"players.id", uId},
+	}
+
+	cur, err := ss.coll.Find(db.Ctx, filter)
+	if err != nil {
+		return nil, err
+	}
+	defer cur.Close(db.Ctx)
+
+	var scorecards []Scorecard
+	if err = cur.All(db.Ctx, &scorecards); err != nil {
+		return nil, err
+	}
+
+	if scorecards == nil {
+		scorecards = make([]Scorecard, 0)
+	}
+
+	return scorecards, nil
 }
