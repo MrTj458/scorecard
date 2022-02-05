50b0e237c16474865b4a18c99debe15211ed3573
MrTj458 <mrtj458@gmail.com>
2022-02-04 20:42:44 -0700

    Switched login and signup forms to react-hook-forms

--- client/src/pages/LoginPage.js
+++ client/src/pages/LoginPage.js
@@ -1,5 +1,6 @@
 import axios from "axios"
 import { useContext, useState } from "react"
+import { useForm } from "react-hook-form"
 import toast from "react-hot-toast"
 import { useLocation, useNavigate } from "react-router-dom"
 import UserContext from "../context/UserContext"
@@ -8,32 +9,41 @@ export default function LoginPage() {
   const navigate = useNavigate()
   const location = useLocation()
   const { setUser } = useContext(UserContext)
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm()
 
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  const [error, setError] = useState("")
+  const [authError, setAuthError] = useState("")
 
   let from = location.state?.from?.pathname || "/app/scorecards"
 
-  const handleSubmit = async (e) => {
-    e.preventDefault()
+  const onSubmit = async (data) => {
     try {
-      const res = await axios.post("/api/users/login", { email, password })
+      const res = await axios.post("/api/users/login", {
+        email: data.email,
+        password: data.password,
+      })
       setUser(res.data)
       navigate(from, { replace: true })
       toast.success(`Signed in as ${res.data.username}`)
     } catch (e) {
       if (e.response.status === 401) {
-        setError("Incorrect email or password")
+        setAuthError("Incorrect email or password")
       } else {
         console.error(e)
+        toast.error("Error signing in, please try again.")
       }
     }
   }
 
   return (
     <div className="mt-5">
-      <form onSubmit={handleSubmit} className="border-2 border-gray-300">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="border-2 border-gray-300"
+      >
         <fieldset className="flex flex-col items-center p-4">
           <legend className="w-full mt-2 text-xl font-bold text-center">
             Log In
@@ -44,13 +54,15 @@ export default function LoginPage() {
               Email
             </label>
             <input
-              required
+              {...register("email", { required: true })}
               name="email"
-              type="email"
-              className="border-2 border-gray-300 p-1"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
+              className={`border-2 border-gray-300 p-1 ${
+                errors.email ? "border-red-600" : ""
+              }`}
             />
+            <small className="block text-red-600">
+              {errors.email && "Email is required."}
+            </small>
           </div>
 
           <div>
@@ -58,16 +70,19 @@ export default function LoginPage() {
               Password
             </label>
             <input
-              required
+              {...register("password", { required: true })}
               name="password"
               type="password"
-              className="border-2 border-gray-300 p-1"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
+              className={`border-2 border-gray-300 p-1 ${
+                errors.email ? "border-red-600" : ""
+              }`}
             />
+            <small className="block text-red-600">
+              {errors.password && "Password is required."}
+            </small>
           </div>
 
-          {error && <p className="mt-2 text-red-600">{error}</p>}
+          {authError && <p className="mt-2 text-red-600">{authError}</p>}
 
           <button
             type="submit"
--- client/src/pages/SignUpPage.js
+++ client/src/pages/SignUpPage.js
@@ -1,5 +1,6 @@
 import axios from "axios"
 import { useContext, useState } from "react"
+import { useForm } from "react-hook-form"
 import toast from "react-hot-toast"
 import { useNavigate } from "react-router-dom"
 import UserContext from "../context/UserContext"
@@ -7,22 +8,26 @@ import UserContext from "../context/UserContext"
 export default function SignUpPage() {
   const navigate = useNavigate()
   const { setUser } = useContext(UserContext)
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm()
 
-  const [username, setUsername] = useState("")
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState("")
-  const [error, setError] = useState("")
+  const [registerError, setRegisterError] = useState("")
 
-  const handleSubmit = async (e) => {
-    e.preventDefault()
-
+  const onSubmit = async (data) => {
     try {
-      const res = await axios.post("/api/users", { username, email, password })
+      const res = await axios.post("/api/users", {
+        username: data.username,
+        email: data.email,
+        password: data.password,
+      })
       setUser(res.data)
       navigate("/app/scorecards", { replace: true })
     } catch (e) {
       if (e.response.status === 422) {
-        setError(e.response.data.detail)
+        setRegisterError(e.response.data.detail)
       } else {
         console.error(e)
         toast.error("Error creating new user, please try again.")
@@ -32,7 +37,10 @@ export default function SignUpPage() {
 
   return (
     <div className="mt-5">
-      <form onSubmit={handleSubmit} className="border-2 border-gray-300">
+      <form
+        onSubmit={handleSubmit(onSubmit)}
+        className="border-2 border-gray-300"
+      >
         <fieldset className="flex flex-col items-center p-4">
           <legend className="w-full mt-2 text-xl font-bold text-center">
             Sign Up
@@ -43,13 +51,16 @@ export default function SignUpPage() {
               Username
             </label>
             <input
-              required
+              {...register("username", { required: true })}
               name="username"
               type="text"
-              className="border-2 border-gray-300 p-1"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
+              className={`border-2 border-gray-300 p-1 ${
+                errors.username ? "border-red-600" : ""
+              }`}
             />
+            <small className="block text-red-600">
+              {errors.username && "Username is required."}
+            </small>
           </div>
 
           <div>
@@ -57,13 +68,16 @@ export default function SignUpPage() {
               Email
             </label>
             <input
-              required
+              {...register("email", { required: true })}
               name="email"
               type="email"
-              className="border-2 border-gray-300 p-1"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
+              className={`border-2 border-gray-300 p-1 ${
+                errors.email ? "border-red-600" : ""
+              }`}
             />
+            <small className="block text-red-600">
+              {errors.email && "Email is required."}
+            </small>
           </div>
 
           <div>
@@ -71,16 +85,21 @@ export default function SignUpPage() {
               Password
             </label>
             <input
-              required
+              {...register("password", { required: true })}
               name="password"
               type="password"
-              className="border-2 border-gray-300 p-1"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
+              className={`border-2 border-gray-300 p-1 ${
+                errors.password ? "border-red-600" : ""
+              }`}
             />
+            <small className="block text-red-600">
+              {errors.password && "Password is required."}
+            </small>
           </div>
 
-          {error && <p className="mt-2 text-red-600">{error}</p>}
+          {registerError && (
+            <p className="mt-2 text-red-600">{registerError}</p>
+          )}
 
           <button
             type="submit"
